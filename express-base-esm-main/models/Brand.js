d105526da7e712755495d12fd1fd9174e919ef47
Cloud <cvteyai9@gmail.com>
2024-08-13 17:10:01 +0800

    cloud部分

--- express-base-esm-main/models/Brand.js
+++ express-base-esm-main/models/Brand.js
@@ -13,14 +13,6 @@ export default async function (sequelize) {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      img: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      info: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
     },
     {
       tableName: 'brand', //直接提供資料表名稱
