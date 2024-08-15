534b367533da1062d5f307ff795bad42494a5dfb
sunenyi <ne199406@gmail.com>
2024-08-15 11:27:51 +0800

    測試next登入

--- next-bs5-main/hooks/useAuth.js
+++ next-bs5-main/hooks/useAuth.js
@@ -8,7 +8,31 @@ const useAuth = () => {
   // 提供給其他程式使用
   const login = async (email, password) => {
     let token, error
-    const url = 'http://localhost:3005/api/users'
+    const url = 'http://localhost:3005/api/my-users/login'
+    // 模擬表單送出
+    const formData = new FormData()
+    formData.append('email', email)
+    formData.append('password', password)
+
+    token = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+      .catch((err) => {
+        error = err
+        return undefined
+      })
+    console.log(token)
+    if (error) {
+      return
+    }
+    if (token) {
+      // setToken(token)
+    }
   }
   const logout = () => {}
 
