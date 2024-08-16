6db33677033829a1a48910d63f5233113f6981cf
Cloud <cvteyai9@gmail.com>
2024-08-16 19:12:15 +0800

    收藏功能-商品列表
    1. 商品列表新增收藏、取消收藏功能
    2. 修改後端相關產品，亂數函式邏輯
    3. 新增我的最愛分頁元件search-nav
    5. 調整商品列表loading動畫出現時機

--- express-base-esm-main/routes/my_products.js
+++ express-base-esm-main/routes/my_products.js
@@ -99,10 +99,14 @@ router.get('/', async (req, res) => {
     // 取出商品資料
     const [rows] = await db.query(queryCluse)
     const totalData = rows.length
+    // 將初始資料擴展一個紀錄是否被加入收藏的屬性值 fav，並設為false
+    const nextData = rows.map((v) => {
+      return { ...v, fav: false }
+    })
     // 計算分頁資訊
     totalPage = Math.ceil(rows.length / perpage)
     // 商品資料為 全部資料用slice方法切割 第一頁為 (start(0), end(12))
-    product.data = rows.slice(start, end)
+    product.data = nextData.slice(start, end)
     product.totalPage = totalPage
     product.teaFilter = teaFilter
     product.brandFilter = brandFilter
@@ -113,125 +117,181 @@ router.get('/', async (req, res) => {
     return res.status(200).json({ status: 'success', product: product })
   } catch (error) {
     console.error('Error executing query', error)
-    res.status(500).json({ error: 'Internal Server Error' })
+    return res.status(404).json({ error: 'All Product Not Found' })
   }
 })
 
 // 取得相關產品
 router.get('/relation_product/:id', async (req, res) => {
-  const currentProductID = Number(req.params.id)
-  const product = await db.execute(
-    `SELECT tea_id FROM my_products WHERE id = ?`,
-    [currentProductID]
-  )
-  // console.log(product[0][0].tea_id)
-  const { tea_id } = product[0][0]
-  const [rows] = await db.execute(
-    'SELECT id, product_name, price, paths FROM my_products WHERE tea_id = ?',
-    [tea_id]
-  )
-  // 設定亂數取資料
-  let randomNumber = []
-  // 設定一個function產生隨機亂數
-  function getRandom() {
-    let tmp = Math.floor(Math.random() * rows.length)
-    if (randomNumber.includes(tmp)) {
-      getRandom()
-    } else {
-      randomNumber.push(tmp)
+  try {
+    const currentProductID = Number(req.params.id)
+    const product = await db.execute(
+      `SELECT tea_id FROM my_products WHERE id = ?`,
+      [currentProductID]
+    )
+    // console.log(product[0][0].tea_id)
+    const { tea_id } = product[0][0]
+    const [rows] = await db.execute(
+      'SELECT id, product_name, price, paths FROM my_products WHERE tea_id = ?',
+      [tea_id]
+    )
+    // 設定亂數取資料，Set物件可讓你儲存任何類型的唯一值（unique）
+    // Set 對象是數值的收集器。你可以按插入順序迭代收集器中的元素。在 Set 裡的元素只會出現一次；意即在 Set 裡的元素都是獨一無二
+    let randomNumber = new Set()
+    // 設定一個while迴圈產生隨機亂數
+    // !!
+    while (randomNumber.size < Math.min(rows.length, 6)) {
+      randomNumber.add(Math.floor(Math.random() * rows.length))
     }
-    if (randomNumber.length < 6) {
-      getRandom()
-    }
-    return randomNumber
+    // 將隨機亂數陣列的值(v)當作rows的索引，再將rows[v]的物件值放到relation_product中，對應的索引(i)位置
+    let relation_product = []
+    // !!
+    randomNumber.forEach((v, i) => {
+      relation_product.push(rows[v])
+    })
+    // console.log(randomNumber)
+    // console.log(relation_product)
+    return res.status(200).json(relation_product)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({ error: 'Relation Product Not Found' })
   }
-  getRandom()
-  // 將隨機亂數陣列的值(v)當作rows的索引，再將rows[v]的物件值放到relation_product中，對應的索引(i)位置
-  let relation_product = []
-  randomNumber.map((v, i) => {
-    relation_product[i] = rows[v]
-  })
-  // console.log(randomNumber)
-  // console.log(relation_product)
-  return res.json(relation_product)
+})
+
+// 請求收藏資料
+router.get('/favorites', async (req, res) => {
+  try {
+    const user_id = req.query.id || 0
+    const [rows] = await db.execute(
+      `SELECT product_id FROM favorites WHERE user_id = ?`,
+      [user_id]
+    )
+    let favoriteProduct = []
+    rows.map((v) => {
+      return favoriteProduct.push(v.product_id)
+    })
+    // console.log(rows)
+    return res.status(200).json(favoriteProduct)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({ error: 'Favorite Product Not Found' })
+  }
+})
+
+router.put('/favorites', async (req, res) => {
+  try {
+    const user_id = req.query.user_id
+    const product_id = req.query.product_id
+    await db.execute(
+      'INSERT INTO favorites (user_id, product_id) VALUES( ?, ?)',
+      [user_id, product_id]
+    )
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ error: 'Invalid input data' })
+  }
+})
+
+router.delete('/favorites', async (req, res) => {
+  try {
+    const user_id = req.query.user_id
+    const product_id = req.query.product_id
+    await db.execute(
+      'DELETE FROM favorites WHERE user_id = ? && product_id = ?',
+      [user_id, product_id]
+    )
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ error: 'Product Not Found' })
+  }
 })
 
 router.get('/reviews/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const [rows] = await db.query(
-    `SELECT id,comment, rating ,created_at FROM reviews WHERE product_id = ${id} ORDER BY created_at DESC`
-  )
-  // console.log(rows.length)
-  // 設定各星級佔存
-  let a = 0,
-    b = 0,
-    c = 0,
-    d = 0,
-    e = 0
-  // 計算各星級數量
-  rows.map((v, i) => {
-    switch (v.rating) {
-      case 1:
-        a++
-        break
-      case 2:
-        b++
-        break
-      case 3:
-        c++
-        break
-      case 4:
-        d++
-        break
-      case 5:
-        e++
-        break
-    }
-  })
-  // 計算總星級數， toFixed(1) 取到小數點第一位(四捨五入)
-  let allRating = (
-    (a * 1 + b * 2 + c * 3 + d * 4 + e * 5) /
-    rows.length
-  ).toFixed(1)
-  // 計算各星級 % 數
-  const starArray = [
-    Math.round((e / rows.length) * 100),
-    Math.round((d / rows.length) * 100),
-    Math.round((c / rows.length) * 100),
-    Math.round((b / rows.length) * 100),
-    Math.round((a / rows.length) * 100),
-  ]
-  const reviews = {}
-  reviews.allData = rows
-  reviews.someData = rows.slice(0, 3)
-  reviews.eachRating = starArray
-  reviews.allRating = allRating
-  reviews.allLength = rows.length
-  // console.log(reviews)
-  return res.json(reviews)
+  try {
+    const id = Number(req.params.id)
+    const [rows] = await db.query(
+      `SELECT id,comment, rating ,created_at FROM reviews WHERE product_id = ${id} ORDER BY created_at DESC`
+    )
+    // console.log(rows.length)
+    // 設定各星級佔存
+    let a = 0,
+      b = 0,
+      c = 0,
+      d = 0,
+      e = 0
+    // 計算各星級數量
+    rows.map((v, i) => {
+      switch (v.rating) {
+        case 1:
+          a++
+          break
+        case 2:
+          b++
+          break
+        case 3:
+          c++
+          break
+        case 4:
+          d++
+          break
+        case 5:
+          e++
+          break
+      }
+    })
+    // 計算總星級數， toFixed(1) 取到小數點第一位(四捨五入)
+    let allRating = (
+      (a * 1 + b * 2 + c * 3 + d * 4 + e * 5) /
+      rows.length
+    ).toFixed(1)
+    // 計算各星級 % 數
+    const starArray = [
+      Math.round((e / rows.length) * 100),
+      Math.round((d / rows.length) * 100),
+      Math.round((c / rows.length) * 100),
+      Math.round((b / rows.length) * 100),
+      Math.round((a / rows.length) * 100),
+    ]
+    const reviews = {}
+    reviews.allData = rows
+    reviews.someData = rows.slice(0, 3)
+    reviews.eachRating = starArray
+    reviews.allRating = allRating
+    reviews.allLength = rows.length
+    // console.log(reviews)
+    return res.status(200).json(reviews)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({ error: 'Reviews Not Found' })
+  }
 })
 router.get('/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const [rows] = await db.query(
-    `SELECT my_products.id AS product_id, product_name, description, price, tea.name AS tea_name, brand.name AS brand_name, package_category.name AS pc_name, style.name AS style_name FROM my_products
-    JOIN tea ON tea.id = my_products.tea_id 
-    JOIN brand ON brand.id = my_products.brand_id 
-    JOIN package_category ON package_category.id = my_products.package_id 
-    JOIN style ON style.id = my_products.style_id 
-    WHERE my_products.id = ${id}
-    `
-  )
-  const [imageRows] = await db.query(
-    `SELECT path FROM product_images JOIN my_products ON my_products.id = product_images.product_id WHERE product_images.product_id = ${id}`
-  )
-  let image = []
-  imageRows.map((v, i) => {
-    image.push(v.path)
-    return image
-  })
-  // console.log(image)
-  const product = { data: rows, images: image }
-  return res.json(product)
+  try {
+    const id = Number(req.params.id)
+    const [rows] = await db.query(
+      `SELECT my_products.id AS product_id, product_name, description, price, tea.name AS tea_name, brand.name AS brand_name, package_category.name AS pc_name, style.name AS style_name FROM my_products
+      JOIN tea ON tea.id = my_products.tea_id 
+      JOIN brand ON brand.id = my_products.brand_id 
+      JOIN package_category ON package_category.id = my_products.package_id 
+      JOIN style ON style.id = my_products.style_id 
+      WHERE my_products.id = ${id}
+      `
+    )
+    const [imageRows] = await db.query(
+      `SELECT path FROM product_images JOIN my_products ON my_products.id = product_images.product_id WHERE product_images.product_id = ${id}`
+    )
+    let image = []
+    imageRows.map((v, i) => {
+      image.push(v.path)
+      return image
+    })
+    // console.log(image)
+    const product = { data: rows, images: image }
+    return res.status(200).json(product)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json('Product Not Found')
+  }
 })
 
 // 取篩選資料的函式
