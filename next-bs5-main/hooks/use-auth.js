7497ce9ee2b22814d4d0ba10ba04d16ed0ec3e71
sunenyi <ne199406@gmail.com>
2024-08-21 15:44:05 +0800

    註冊測試

--- next-bs5-main/hooks/use-auth.js
+++ next-bs5-main/hooks/use-auth.js
@@ -31,11 +31,9 @@ const AuthContext = createContext({ auth: {}, setUser: {} })
 // 只需要必要的資料即可，沒有要多個頁面或元件用的資料不需要加在這裡
 // !!注意JWT存取令牌中只有id, username, google_uid, line_uid在登入時可以得到
 export const initUserData = {
-  id: 0,
+  id: '',
   user_name: '',
   google_uid: '',
-  // line_uid: '',
-  // name: '',
   email: '',
 }
 
@@ -109,15 +107,15 @@ export const AuthProvider = ({ children }) => {
   }
 
   // didMount(初次渲染)後，向伺服器要求檢查會員是否登入中
-  useEffect(() => {
-    if (router.isReady && !auth.isAuth) {
-      handleCheckAuth()
-    }
-    // 下面加入router.pathname，是為了要在向伺服器檢查後，
-    // 如果有比對到是隱私路由，就執行跳轉到登入頁面工作
-    // 注意有可能會造成向伺服器要求多次，此為簡單的實作範例
-    // eslint-disable-next-line
-  }, [router.isReady, router.pathname])
+  // useEffect(() => {
+  //   if (router.isReady && !auth.isAuth) {
+  //     handleCheckAuth()
+  //   }
+  //   // 下面加入router.pathname，是為了要在向伺服器檢查後，
+  //   // 如果有比對到是隱私路由，就執行跳轉到登入頁面工作
+  //   // 注意有可能會造成向伺服器要求多次，此為簡單的實作範例
+  //   // eslint-disable-next-line
+  // }, [router.isReady, router.pathname])
 
   return (
     <AuthContext.Provider
