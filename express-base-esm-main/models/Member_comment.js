0d635787c305f5b70033ccbcb7d746c2a90b49ac
shane0110 <161286297+shane0110@users.noreply.github.com>
2024-08-22 14:34:46 +0800

    目前進度
    1.目前做到詳細頁面剩下評論跟隨機課程

--- express-base-esm-main/models/Member_comment.js
+++ express-base-esm-main/models/Member_comment.js
@@ -2,20 +2,36 @@ import { DataTypes } from 'sequelize'
 
 export default async function (sequelize) {
   return sequelize.define(
-    'Location',
+    'Member_comment',
     {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      name: {
-        type: DataTypes.STRING,
+      member_id: {
+        type: DataTypes.INTEGER,
         allowNull: false,
+      },
+      course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      comment: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: true,
       },
     },
     {
-      tableName: 'location', //直接提供資料表名稱
+      tableName: 'member_comment', //直接提供資料表名稱
       timestamps: true, // 使用時間戳
       paranoid: false, // 軟性刪除
       underscored: true, // 所有自動建立欄位，使用snake_case命名
